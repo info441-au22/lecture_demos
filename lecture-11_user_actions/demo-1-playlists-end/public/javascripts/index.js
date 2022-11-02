a3ea7cf81d0a6cd65fcfc84c1378a2d791788104
giselejfox <72312641+giselejfox@users.noreply.github.com>
2022-11-02 13:51:00 -0700

    test push

--- lecture-11_user_actions/demo-1-playlists-end/public/javascripts/index.js
+++ lecture-11_user_actions/demo-1-playlists-end/public/javascripts/index.js
@@ -1,1 +1,12 @@
 //TODO: Fill in actions
+
+function addUSer(){
+    // need to get the name someone typed in from the username
+    let name = document.getElementById("name_input").value 
+
+    await fetch("/api/v1/users", {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({username: name})
+    })
+}
